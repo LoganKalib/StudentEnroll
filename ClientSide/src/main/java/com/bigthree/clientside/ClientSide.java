f712fd483c3c02636f29dda443309458f47638be
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2023-09-21 15:34:59 +0200

    base project
    added base project

---
+++ ClientSide/src/main/java/com/bigthree/clientside/ClientSide.java
@@ -0,0 +1,12 @@
+package com.bigthree.clientside;
+
+/**
+ *
+ * @author logan
+ */
+public class ClientSide {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ serverSide/src/main/java/com/bigthree/serverside/ServerSide.java
@@ -0,0 +1,12 @@
+package com.bigthree.serverside;
+
+/**
+ *
+ * @author logan
+ */
+public class ServerSide {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
